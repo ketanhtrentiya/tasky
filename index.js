eb61b03434abfb6bfa3f9f3b04a9ac7fdee48866
Ketan Trentiya <ketanhtrentiya@gmail.com>
2024-04-12 16:32:23 +0530

    Integration of Electron Window

---
+++ index.js
@@ -0,0 +1,17 @@
+const electron = require("electron");
+
+const { app, BrowserWindow } = electron;
+
+// GLOBAL
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    height: 500,
+    width: 300,
+    frame: false,
+    resizable: false,
+  });
+
+  mainWindow.loadURL(`http://localhost:3000`);
+});
