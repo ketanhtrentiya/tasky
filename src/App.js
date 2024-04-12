9752cd9553de91001b7a28d41333f247a678f2ba
Ketan Trentiya <ketanhtrentiya@gmail.com>
2024-04-12 16:22:55 +0530

    React App via create-react-app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+      <div style={{
+        display: "flex",
+        justifyContent: "center"
+      }}>
+        <h1>To Do App</h1>
+      </div>
+      <div>
+        <ul>
+          <li>Apple</li>
+          <li>Samsung</li>
+          <li>Nokia</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
